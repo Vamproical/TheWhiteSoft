405518d8225aa8cc5cb8389a68acf24ea10e4007
Nick <mimoun136@yandex.ru>
2022-02-27 18:33:00 +1000

    Add API GET response.

--- src/main/java/assignment/JsonUtils.java
+++ src/main/java/assignment/JsonUtils.java
@@ -5,17 +5,41 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
+import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FileUtils {
+public class JsonUtils {
 
-    private FileUtils() {
+    private JsonUtils() {
         throw new IllegalStateException("Utility class");
+    }
+
+    public static List<String> getTextFromURL() {
+        List<String> text = new ArrayList<>();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create("https://raw.githubusercontent.com/thewhitesoft/student-2022-assignment/main/data.json"))
+                .GET()
+                .build();
+        try {
+            var response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            JsonArray parser = JsonParser.parseString(response.body()).getAsJsonArray();
+            for (JsonElement r : parser) {
+                text.add(r.getAsString());
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return text;
     }
 
     public static List<String> readErrorText(String inputFile) {
--- src/main/java/assignment/Main.java
+++ src/main/java/assignment/Main.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> textWithError = FileUtils.readErrorText("src/main/resources/data.json");
-        List<Replacements> replacements = FileUtils.readReplacementFile("src/main/resources/replacement.json");
+        List<String> textWithError = JsonUtils.getTextFromURL();
+        //List<String> textWithError = FileUtils.readErrorText("src/main/resources/data.json");
+        List<Replacements> replacements = JsonUtils.readReplacementFile("src/main/resources/replacement.json");
         ErrorCorrecting errorCorrecting = new ErrorCorrecting();
-        List<String> result = errorCorrecting.correct(textWithError, replacements);
-        FileUtils.writeFile(result);
+        JsonUtils.writeFile(errorCorrecting.correct(textWithError, replacements));
     }
 }
